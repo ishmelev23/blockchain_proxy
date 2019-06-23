ca6abe9f70ec81a0a82651fc6a83c936c1850773
Ilya Shmelev <ishmelev23@gmail.com>
2019-06-23 21:27:51 +0300

    Added errors processing and function mocks

--- src/api/blueprints/transactions.py
+++ src/api/blueprints/transactions.py
@@ -1,12 +1,21 @@
 from flask import Blueprint, request
 from logging import getLogger
 
-from src.api.errors import ERROR_CONTRACT_NOT_SUPPORTED, ERROR_FUNCTION_NOT_SUPPORTED, ERROR_FUNCTION_INVALID_FIELDS
+from src.api.errors import ERROR_CONTRACT_NOT_SUPPORTED, ERROR_FUNCTION_NOT_SUPPORTED, ERROR_FUNCTION_INVALID_FIELDS, \
+    ERROR_FUNCTION_CALL, ERROR_TRANSACTION_DB_SAVE
 from src.api.formatter import format_response
 from src.api.forms import NAME2FORM
 
 logger = getLogger(__name__)
 blueprint = Blueprint('transactions', __name__)
+
+
+def send_transaction(data: dict) -> str:
+    return '123'
+
+
+def save_transaction(trx_hash: str):
+    pass
 
 
 @blueprint.route('/contracts/<string:contract_name>/<string:func_name>', methods=['POST'])
@@ -24,7 +33,19 @@ def transactions_create(contract_name: str, func_name: str):
     if not form.validate():
         return format_response(data={'errors': form.errors}, error_code=ERROR_FUNCTION_INVALID_FIELDS)
 
-    return format_response(data={})
+    try:
+        trx_hash = send_transaction(request.json)
+    except Exception as e:
+        logger.critical("Can't propagate data to blockchain!")
+        return format_response(data={}, error_code=ERROR_FUNCTION_CALL)
+
+    try:
+        save_transaction(request.json)
+    except Exception as e:
+        logger.critical("Can't save data to local database. Transaction with hash %s is not tracking!" % trx_hash)
+        return format_response(data={'trx_hash': trx_hash}, error_code=ERROR_TRANSACTION_DB_SAVE)
+
+    return format_response(data={'trx_hash': trx_hash})
 
 
 @blueprint.route('/transactions/<string:trx_hash>', methods=['GET'])
--- src/api/errors.py
+++ src/api/errors.py
@@ -3,11 +3,14 @@ ERROR_INTERNAL_SERVER_ERROR = 0
 ERROR_CONTRACT_NOT_SUPPORTED = 100
 ERROR_FUNCTION_NOT_SUPPORTED = 101
 ERROR_FUNCTION_INVALID_FIELDS = 102
+ERROR_FUNCTION_CALL = 103
+ERROR_TRANSACTION_DB_SAVE = 104
 
 ERROR_MESSAGES = {
     ERROR_INTERNAL_SERVER_ERROR: "Internal server error",
     ERROR_CONTRACT_NOT_SUPPORTED: "Contract '{contract_name}' does not supported",
     ERROR_FUNCTION_NOT_SUPPORTED: "Function '{func_name} for contract '{contract_name}' doest not supported",
-    ERROR_FUNCTION_INVALID_FIELDS: "Function call data contains errors: {errors}"
-
+    ERROR_FUNCTION_INVALID_FIELDS: "Function call data contains errors: {errors}",
+    ERROR_FUNCTION_CALL: "Failed to propagate data to blockchain",
+    ERROR_TRANSACTION_DB_SAVE: "Failed to track transaction with hash {trx_hash} to database"
 }

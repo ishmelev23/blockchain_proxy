e9cb873738333804778ca8ffa6048c80cd725e85
Ilya Shmelev <ishmelev23@gmail.com>
2019-06-24 21:24:00 +0300

    Added database tests

--- tests/base.py
+++ tests/base.py
@@ -1,10 +1,14 @@
-import os
 from unittest import TestCase
 
+import settings
 from app import app
 
 
 class ApiTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        assert settings.TESTING, "Trying to start tests in non testing mode. You should setup TESTING env variable"
 
     def setUp(self) -> None:
         app.config['TESTING'] = True
